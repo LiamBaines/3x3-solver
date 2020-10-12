80b7e8000494dc8565226c3904dbd8f3b79d2531
Liam Baines <liam@thebaines.com>
2020-10-12 20:43:19 +0100

    Lehmer hash first commit

--- empty.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-fs.writeFile('corners.csv', 'ABCDEFGH,0,', (err) => {
-  if (err) throw err;
-  console.log('corners.csv cleared');
-});
--- readstream3.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-let reader = fs.createReadStream('data.csv', {
-  flag: 'a+',
-  encoding: 'ascii',
-  highWaterMark: 64
-});
-
-console.log(reader.read(4))
