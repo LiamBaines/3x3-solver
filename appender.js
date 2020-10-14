61637b0ca004d28528ccf7dc9b51cc497a51e308
Liam Baines <liam@thebaines.com>
2020-10-14 16:57:31 +0100

    Tweaks

--- appender.js
+++ appender.js
@@ -1,7 +1,6 @@
 const cube = require('./cornercube.js')
 const hash = require('./hash.js')
 const t = {
-  X: ['U', 'u', 'D', 'd', 'L', 'l', 'R', 'r', 'F', 'f', 'B', 'b'],
   U: ['U', 'D', 'd', 'L', 'l', 'R', 'r', 'F', 'f', 'B', 'b'],
   u: ['D', 'd', 'L', 'l', 'R', 'r', 'F', 'f', 'B', 'b'],
   D: ['D', 'L', 'l', 'R', 'r', 'F', 'f', 'B', 'b'],
@@ -13,33 +12,40 @@ const t = {
   F: ['U', 'u', 'D', 'd', 'L', 'l', 'R', 'r', 'F', 'B', 'b'],
   f: ['U', 'u', 'D', 'd', 'L', 'l', 'R', 'r', 'B', 'b'],
   B: ['U', 'u', 'D', 'd', 'L', 'l', 'R', 'r', 'B'],
-  b: ['U', 'u', 'D', 'd', 'L', 'l', 'R', 'r']
+  b: ['U', 'u', 'D', 'd', 'L', 'l', 'R', 'r'],
+  X: ['U', 'u', 'D', 'd', 'L', 'l', 'R', 'r', 'F', 'f', 'B', 'b'],
 }
 
 // initialise pattern database
 let pdb = new Array(88179840)
-pdb[0] = 'X';
+pdb[0] = 0;
 
 // add first item to pattern database
-let queue = [[0, 0, 'X']] // = [[z, d, last]]
+let queue = [[0, 'X']] // = [[z, d, last]]
+//let queue = [[[[0, 1, 2, 3, 4, 5, 6, 7], [0, 0, 0, 0, 0, 0, 0, 0]], 'X']]
 let turns = 0;
 let inserts = 0;
-let D = 0;
+let d = 0;
 
 //extend tree up to a given limit
 function execute(lim) {
-  //let startTime = Date.now()
-  while (D < lim) {
-    let origState = hash.getState(queue[0][0]);
-    D = queue[0][1]
-    let last = queue[0][2]
-    if (D < lim) {
+  let queue = [[0, 'X']]
+  let queue = [[0, 0]]
+  let startTime = Date.now()
+  while (d < lim) {
+    let z = queue[0][0]
+    let origState = hash.getState(z);
+    //let origState = queue[0][0]
+    d = pdb[z]
+    let last = queue[0][1]
+    if (d < lim) {
       t[last].forEach(trn => {
         let newState = cube.turn(origState, trn)
-        let z = hash.getZ(newState)
-        if (pdb[z] === undefined) {
-          pdb[z] = D + 1
-          queue.push([z, D + 1, trn])
+        let Z = hash.getZ(newState)
+        if (pdb[Z] === undefined) {
+          pdb[Z] = d + 1
+          queue.push([Z, trn])
+          //queue.push([newState, trn])
           inserts++ 
         }
         turns++
@@ -50,18 +56,26 @@ function execute(lim) {
   let endTime = Date.now()
   let timeTaken = endTime - startTime;
   console.log(`Executed to depth ${lim} in ${timeTaken} ms (${Math.floor(turns/timeTaken)} turns/ms). ${turns} turns, ${inserts} inserts (${Math.floor(100*(turns - inserts)/turns)}% redundancy)`)
-  for (h = 0; h < 10; h++) {
-    check(lim)
+  for (h = 1; h < lim + 1; h++) {
+    check(h)
   }
 }
 
+let tries = 0;
+let insertsC = 0;
+
 function combine(n, state = [[0, 1, 2, 3, 4, 5, 6, 7], [0, 0, 0, 0, 0, 0, 0, 0]], last = 'X', d = 0) {
   let z = hash.getZ(state)
-  if (pdb[z] === undefined || pdb[z] > d) pdb[z] = d;
+  if (pdb[z] === undefined || pdb[z] > d) {
+    pdb[z] = d;
+    insertsC++
+  } 
+  tries++
   if (d < n) {
     t[last].forEach(trn => {
       let newState = cube.turn(state, trn)
       combine(n, newState, trn, d + 1)
+      turns++
     })
   }
 }
@@ -69,10 +83,12 @@ function combine(n, state = [[0, 1, 2, 3, 4, 5, 6, 7], [0, 0, 0, 0, 0, 0, 0, 0]]
 function check(n) {
   let state = [[0, 1, 2, 3, 4, 5, 6, 7], [0, 0, 0, 0, 0, 0, 0, 0]]
   let str = ''
+  let last = 'X'
   for (j = 0; j < n; j++) {
-    let ran = Math.floor(Math.random() * 12)
-    state = cube.turn(state, t.X[ran])
-    str = str.concat(t.X[ran])
+    let ran = Math.floor(Math.random() * t[last].length)
+    state = cube.turn(state, t[last][ran])
+    str = str.concat(t[last][ran])
+    last = t[last][ran]
   }
   x = hash.encode(state[0])
   y =  hash.dec2tern(state[1])
@@ -85,10 +101,11 @@ function check(n) {
 function executeCombine(n) {
   let startTime = Date.now()
   combine(n)
+  let endTime = Date.now();
   for (h = 1; h < n + 1; h++) {
     check(h)
   }
-  console.log(`Extended to depth ${n} in ${Date.now() - startTime}ms`)
+  console.log(`Extended to depth ${n} (${turns} turns) in ${endTime - startTime}ms. Redundancy rate: ${Math.floor(100*((tries - insertsC)/tries))}%`)
 }
 
-executeCombine(7)
+execute(5)
